dfd122b51e1a29d8d69b7f78c0de5ef0c16b89d8
CamilaSantos <camscatt17@gmail.com>
2020-08-05 11:56:33 -0300

    Dia 3

---
+++ scripts/addField.js
@@ -0,0 +1,24 @@
+//Procurar o botão
+document.querySelector("#add-time")
+
+//Quando clicar no botão
+.addEventListener('click', cloneField);
+
+//Executar uma ação
+function cloneField() {
+    //Duplicar os campos? Que campos?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    //Pegar os campos. Que campos?
+    const fields = newFieldContainer.querySelectorAll('input')
+   
+    //para cada campo, limpar
+    fields.forEach(function(field){
+        //pegar o field do momento e limpa ele
+        field.value = ""
+    })
+
+    //Colocar na página: onde?
+    document.querySelector('#schedule-items').appendChild(fields)
+}
+    
