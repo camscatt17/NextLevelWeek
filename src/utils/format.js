f2d929c60b2739d6aebc564f58cf621811ca6474
CamilaSantos <camscatt17@gmail.com>
2020-08-08 00:08:38 -0300

    Dia 5 pt 2

--- src/utils/format.js
+++ src/utils/format.js
@@ -27,8 +27,14 @@ function getSubject(subjectNumber) {
     return subjects[position]
 }
 
+function convertHoursToMinutes(time){
+    const [hour, minutes] = time.split(":")
+    return Number((hour * 60) + minutes)
+}
+
 module.exports = {
     subjects,
     weekdays,
-    getSubject
+    getSubject,
+    convertHoursToMinutes
 }
